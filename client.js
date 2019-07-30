c5774c32cca3619c7b17937ebac67ead5a55e09e
jglascock-at-531265169552 <jesse@etchgis.com>
2019-07-30 08:59:53 -0400

    cliet

--- client.js
+++ client.js
@@ -15,3 +15,26 @@ request(requestSettings, function (error, response, body) {
     else
         console.log(error);
 });
+
+
+// var GtfsRealtimeBindings = require('gtfs-realtime-bindings');
+// var request = require('request');
+
+// var requestSettings = {
+//     method: 'GET',
+//     //url: 'http://localhost:3000/realtime',
+//     url: 'https://etch.app/gtfs/api/realtime',
+//     encoding: null
+// };
+// request(requestSettings, function (error, response, body) {
+//     if (!error && response.statusCode == 200) {
+//         var feed = GtfsRealtimeBindings.FeedMessage.decode(body);
+//         //console.log(feed.entity);
+//         feed.entity.forEach(function (entity) {
+//             if (entity.trip_update) {
+//                 //console.log(entity.trip_update);
+//             }
+//         });
+//     } else
+//         console.log(error);
+// });
